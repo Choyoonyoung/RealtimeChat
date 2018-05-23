5eecb3bd9c89ef2500caf589157823883191be66
yoonyoung <yoon980208@gmail.com>
2018-05-23 11:58:52 +0900

    'RealtimeChat'

---
+++ RealtimeChat/app/src/main/java/com/example/yy/realtimechat/ChatAdapter.java
@@ -0,0 +1,54 @@
+package com.example.yy.realtimechat;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+import java.util.zip.Inflater;
+
+import static com.example.yy.realtimechat.R.layout.listitem_chat;
+
+public class ChatAdapter extends ArrayAdapter<ChatData> {
+    private final SimpleDateFormat mSimpleDataFormat = new SimpleDateFormat("a h:mm", Locale.getDefault());
+
+    public ChatAdapter(@NonNull Context context, int resource) {
+        super(context, resource);
+    }
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.listitem_chat, null);
+
+            viewHolder = new ViewHolder();
+            viewHolder.mTextUserName = (TextView) convertView.findViewById(R.id.txt_userName);
+            viewHolder.mTextMessage = (TextView) convertView.findViewById(R.id.txt_message);
+            viewHolder.mTextTime = (TextView) convertView.findViewById(R.id.txt_time);
+
+            convertView.setTag(viewHolder);
+
+
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        ChatData chatData = getItem(position);
+        viewHolder.mTextUserName.setText(chatData.userName);
+        viewHolder.mTextMessage.setText(chatData.message);
+        viewHolder.mTextTime.setText(mSimpleDataFormat.format(chatData.time));
+
+        return convertView;
+    }
+        private class ViewHolder {
+            private TextView mTextUserName;
+            private TextView mTextMessage;
+            private TextView mTextTime;
+        }
+    }
+
---
+++ RealtimeChat/app/src/main/java/com/example/yy/realtimechat/ChatData.java
@@ -0,0 +1,11 @@
+package com.example.yy.realtimechat;
+
+public class ChatData {
+    public String firebaseKey;
+    public String userName;
+    public String userPhotoUrl;
+    public String userEmail;
+    public String message;
+    public long time;
+
+}
